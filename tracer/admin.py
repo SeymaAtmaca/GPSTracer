1f47ff3322b0776e56bd6c3c0d91d8a2075fd593
SeymaAtmaca <symatmc@hotmail.com>
2024-10-06 23:33:55 +0300

    admin panel restore

--- tracer/admin.py
+++ tracer/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Notification, FriendRequest, Location
 
-admin.site.register(User)
+# admin.site.register(User)
+# admin.site.register(FriendRequest)
+# admin.site.register(Location)
+# admin.site.register(Notification)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','email','first_name','last_name','isActive')
+    list_filter = ('isActive',)
+    
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'message', 'created_at', 'is_read',)
+    list_filter = ('is_read', 'created_at',)
